99a97733c30bfc3742a6ac65c797501da5bbec20
kylindreagan <155770203+kylindreagan@users.noreply.github.com>
2026-01-29 19:26:35 -0500

    Start work on ShapeDNA
    Import all shapes and add libraries

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Spectral-Shape-Analysis
 Senior study topic
+Objects acquired from: SHREC 2011, SHREC 2021
---
+++ ShapeDNA/laplaceBeltramiShape.py
@@ -0,0 +1,6 @@
+#Assistance with creation: https://github.com/raphaelsulzer/ShapeDNA
+import open3d as o3d
+import numpy as np
+from scipy.sparse import csr_matrix
+import math
+from scipy.sparse.linalg import eigsh
